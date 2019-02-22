25129dfec97d259270b7a34b394a121d7dadc3f9
VladislavCat <kshift84@gmail.com>
2019-02-23 01:11:46 +0300

    1.3 JavaDoc

--- chapter_001/src/main/java/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/job4j/calculate/Calculate.java
@@ -1,7 +1,10 @@
 package ru.job4j.calculate;
 
 public class Calculate {
-	
+	/**
+	*Вывод "Hello World"
+	*@author Vladislav Kotov(kshift84@gmail.com)
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
