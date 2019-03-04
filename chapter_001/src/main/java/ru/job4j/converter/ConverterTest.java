a2301fb9df7df4cd36eeca2153f2693554818f57
VladislavCat <kshift84@gmail.com>
2019-03-05 01:56:37 +0300

    Calculator and Converter

--- chapter_001/src/main/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/main/java/ru/job4j/converter/ConverterTest.java
@@ -4,7 +4,7 @@ package ru.job4j.converter;
  */
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
-import static org. junit.Assert.assertThat;
+import static org.junit.Assert.assertThat;
 /**
  * Открытие класса конвертер.тест
  */
