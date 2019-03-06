904763f9a207c551a34d1deff964d9a5f7fc223c
VladislavCat <kshift84@gmail.com>
2019-03-06 20:08:24 +0300

    IdealFit

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+/**
+ * Программа для расчета идеального веса
+ */
+public class Fit {
+    /**
+     * Идеальный вес для мужчины
+     * @param height Рост.
+     * @return Идеальный вес.
+     */
+    public double manWeight(double height) {
+        return (180 - 100) * 1.15;
+    }
+
+    /**
+     * Идеальный вес для женщины
+     * @param height Рост.
+     * @return Идеальный Вес.
+     */
+    public double womanWeight(double height) {
+        return (1700 - 110) * 1.15;
+    }
+}
+
