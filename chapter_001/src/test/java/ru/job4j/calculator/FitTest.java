6baacebb53983f20de14e0a2e93a9ded496bb15c
VladislavCat <kshift84@gmail.com>
2019-03-10 22:07:05 +0300

    IdealFit

--- chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
@@ -10,14 +10,14 @@ public class FitTest {
     @Test
     public void manWeight() {
         Fit fit = new Fit();
-        double weight = fit.manWeight(double height);
+        double weight = fit.manWeight(180);
         assertThat(weight, closeTo(92.0, 0.1));
     }
 
     @Test
     public void womanWeight() {
         Fit fit = new Fit();
-        double weight = fit.womanWeight(double height);
+        double weight = fit.womanWeight(170);
         assertThat(weight, closeTo(69.0, 0.1));
     }
 }
