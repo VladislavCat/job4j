3a267f17ddcb82ec954b33d9f0528f62b7a37b8c
VladislavCat <kshift84@gmail.com>
2019-03-14 20:15:10 +0300

    Point and PointTest

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,11 +1,16 @@
 package ru.job4j.condition;
 
+/**
+ * Открываем класс
+ */
 public class Point {
+    /**
+     * Открываем метод тела программы
+     */
     public double distance(int x1, int y1, int x2, int y2) {
         double first = Math.pow(x2 - x1, 2);
-        double second = Math.pow(y2-y1, 2);
-        double third = Math.sqrt(first+second);
-        return third;
+        double second = Math.pow(y2 - y1, 2);
+        return Math.sqrt(first + second);
     }
 }
 
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -4,8 +4,14 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Открываем класс
+ */
 public class PointTest {
     @Test
+    /**
+     * Открываем метод тестировщика
+     */
     public void whenZeroAndTenThenTen() {
         Point point = new Point();
         double result = point.distance(0, 0, 0, 10);
