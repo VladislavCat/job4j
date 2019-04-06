1c945a9285d010e6182959aedb4974ede619ca3a
VladislavCat <kshift84@gmail.com>
2019-04-06 22:55:34 +0300

    BubleSort and BubleSortTest

--- chapter_001/src/main/java/ru/job4j/array/BubleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubleSort.java
@@ -1,11 +1,17 @@
 package ru.job4j.array;
 
 public class BubleSort {
-     public int sort(int[] array){
-         for(int i=0; i<array.length; i++) {
-
-         }
-
-     }
-
+    public int[] sort(int[] array) {
+        for (int i=array.length-1 ; i>0; i--) {
+            int index=0;
+            for (index = 0; index < array.length-1; index++) {
+                if (array[index] > array[index+1]) {
+                    int temp = array[index];
+                    array[index] = array[index+1];
+                    array[index+1] = temp;
+                }
+            }
+        }
+        return array;
+    }
 }
