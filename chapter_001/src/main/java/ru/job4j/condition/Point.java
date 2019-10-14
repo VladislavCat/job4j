26801767ada4c7c414e7e44a2eae98079c0bd948
VladislavCat <kshift84@gmail.com>
2019-10-15 00:51:50 +0300

    2.1. Рефакторинг - Расстояние между точками.

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,16 +1,26 @@
 package ru.job4j.condition;
-
+public class Point{
 /**
- * Открываем класс
+ * Это поле объекта. Оно доступно только конкретному объекту.
  */
-public class Point {
-    /**
-     * Открываем метод тела программы
-     */
-    public double distance(int x1, int y1, int x2, int y2) {
-        double first = Math.pow(x2 - x1, 2);
-        double second = Math.pow(y2 - y1, 2);
-        return Math.sqrt(first + second);
-    }
+private int x;
+private int y;
+/**
+ * И это поле объекта. Оно доступно только конкретному объекту
+ */
+public Point(int first, int second){
+        this.x=first;
+        this.y=second;
+        }
+public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+        }
+
+public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+   }
 }
 
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -9,12 +9,10 @@ import static org.junit.Assert.assertThat;
  */
 public class PointTest {
     @Test
-    /**
-     * Открываем метод тестировщика
-     */
     public void whenZeroAndTenThenTen() {
-        Point point = new Point();
-        double result = point.distance(0, 0, 0, 10);
-        assertThat(result, is(10D));
+        Point a = new Point(1, 2);
+        Point b = new Point(1, 4);
+        double result = a.distance(b);
+        assertThat(result, is(2D));
     }
 }
