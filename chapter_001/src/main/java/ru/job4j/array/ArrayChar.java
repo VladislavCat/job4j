b1174a3b5cb901ae7648bf55e83bf46e84c38f4b
VladislavCat <kshift84@gmail.com>
2019-10-29 23:35:00 +0300

    1. Конструктор по умолчанию. [#173889]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,19 +1,10 @@
 package ru.job4j.array;
-/**
- * Обертка над строкой.
- */
 public class ArrayChar {
     private char[] data;
 
     public ArrayChar(String line) {
         this.data = line.toCharArray();
     }
-    /**
-     * Проверяет, что слово начинается с префикса.
-     *
-     * @param prefix префикс.
-     * @return если слово начинается с префикса
-     */
     public boolean startWith(String prefix) {
         boolean result = true;
         char[] value = prefix.toCharArray();
---
+++ chapter_001/src/main/java/ru/job4j/oop/Animal.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Animal {
+    public Animal(){
+        super();
+        System.out.println("Animal");
+    }
+    public static void main(String[] args) {
+        Predator predator = new Predator();
+    }
+}
+
