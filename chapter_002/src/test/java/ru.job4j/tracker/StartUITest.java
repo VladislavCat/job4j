0c647fef9f08f7aa8c14077c2d87578e45119a3a
VladislavCat <kshift84@gmail.com>
2020-05-14 19:35:26 +0300

    Используя шаблон проектирования - Тесты

--- chapter_002/src/test/java/ru.job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru.job4j/tracker/StartUITest.java
@@ -7,40 +7,12 @@ import static org.junit.Assert.assertThat;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(), // id сохраненной заявки в объект tracker.
-                "replaced item"
-        };
-        StartUI.replaceBid(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item= new Item("newItem1");
-        Item item2= new Item("newItem2");
-        tracker.add(item);
-        tracker.add(item2);
-        String[] answers = {
-                item.getId()
-        };
-        StartUI.deleteBid(tracker, new StubInput(answers));
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted.getName(), is("null") );
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[]{"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[]{action});
+        assertThat(action.isCall(), is(true));
     }
 }
